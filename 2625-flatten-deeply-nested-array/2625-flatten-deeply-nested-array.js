350a3c288f72510e6c92afb3cd2d3d0f5a16c1d2
Antash00 <33508579+Antash00@users.noreply.github.com>
2026-05-04 16:20:50 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -7,16 +7,16 @@ var flat = function (arr, n) {
     if (n === 0) {
         return arr
     }
-    return flatArray(arr, 0, n)
-}
-var flatArray = function (arr, curDepth, depth) {
-    let flatArrayResult = []
-    for (let el of arr) {
-        if (!Array.isArray(el) || curDepth >= depth) {
-            flatArrayResult.push(el)
-        } else {
-            flatArrayResult.push(...flatArray(el, curDepth + 1, depth))
+    var flatArray = function (arr, curDepth) {
+        let flatArrayResult = []
+        for (let el of arr) {
+            if (!Array.isArray(el) || curDepth >= n) {
+                flatArrayResult.push(el)
+            } else {
+                flatArrayResult.push(...flatArray(el, curDepth + 1))
+            }
         }
+        return flatArrayResult
     }
-    return flatArrayResult
+    return flatArray(arr, 0, n)
 }
