d4f936722aa8a6739243c202dbae889bdda1be2b
Antash00 <33508579+Antash00@users.noreply.github.com>
2026-05-04 16:37:00 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -5,8 +5,8 @@
  */
 var filter = function (arr, fn) {
     const array = []
-    for (let i =0; i<arr.length;i++) {
-        if (fn(arr[i],i)) {
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
             array.push(arr[i])
         }
     }
