7753d54b4320e9d4ecf038709254fc583a449037
Antash00 <33508579+Antash00@users.noreply.github.com>
2026-05-14 13:50:23 +0500

    Time: 67 ms (22.92%), Space: 59.7 MB (52.08%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} poured
+ * @param {number} query_row
+ * @param {number} query_glass
+ * @return {number}
+ */
+var champagneTower = function (poured, query_row, query_glass) {
+    const champTower = Array.from({ length: 101 }, () => Array(101).fill(0))
+    if (poured === 0) {
+        return 0
+    }
+    champTower[0][0] = poured
+    for (let i = 0; i < query_row; i++) {
+        for (let j = 0; j < champTower[i].length; j++) {
+            if (champTower[i][j] > 1) {
+                const extra = champTower[i][j] - 1
+
+                champTower[i][j] = 1
+
+                champTower[i + 1][j] += extra / 2
+                champTower[i + 1][j + 1] += extra / 2
+            }
+        }
+    }
+
+    return champTower[query_row][query_glass] > 1 ? 1 : champTower[query_row][query_glass]
+};
